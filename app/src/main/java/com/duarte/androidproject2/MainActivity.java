8df784aedb54aa257f36c5078892b9ea4e605f38
Braulio Duarte <brauliod617@gmail.com>
2019-11-29 15:53:51 -0500

    clean up

--- app/src/main/java/com/duarte/androidproject2/MainActivity.java
+++ app/src/main/java/com/duarte/androidproject2/MainActivity.java
@@ -1,14 +1,11 @@
 package com.duarte.androidproject2;
 
-        import androidx.appcompat.app.AppCompatActivity;
-        import android.content.Intent;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.view.View;
-        import android.widget.EditText;
-        import android.widget.TextView;
-
-        import java.io.Serializable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity{
 
