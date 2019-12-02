9c8ba3465cca110775281d4247f17f44853be7c6
Braulio Duarte <brauliod617@gmail.com>
2019-12-01 23:32:58 -0500

    start creating questions page

---
+++ app/src/main/java/com/duarte/androidproject2/QuestionsPage.java
@@ -0,0 +1,14 @@
+package com.duarte.androidproject2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuestionsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questions_page);
+    }
+}
